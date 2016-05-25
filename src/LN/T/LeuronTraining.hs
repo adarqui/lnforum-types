bac1432b519a1b0d52863210563ac3e57091f266
Andrew Darqui <andrew.darqui@gmail.com>
2016-05-25 00:56:51 -0400

    some summary types

--- src/LN/T/LeuronTraining.hs
+++ src/LN/T/LeuronTraining.hs
@@ -12,7 +12,9 @@ import           LN.T.Prelude
 
 data LeuronTrainingSummary
   = LTS_View
+  | LTS_Skip
   | LTS_Know
   | LTS_DontKnow
   | LTS_DontCare
+  | LTS_Protest
   deriving (Eq, Ord, Show, Read, Generic, Typeable, Out)
