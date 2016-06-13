1dfd79f2ceae44e2dd0170c37f268170bddc14a8
Andrew Darqui <andrew.darqui@gmail.com>
2016-06-13 08:37:47 -0400

    odsfkdsok

--- src/LN/T/Pack/Group.hs
+++ src/LN/T/Pack/Group.hs
@@ -12,6 +12,7 @@ import           LN.T.Group
 import           LN.T.Group.Response
 import           LN.T.Group.Stat
 import           LN.T.Like.Response
+import           LN.T.Organization.Response
 import           LN.T.Prelude
 import           LN.T.Star.Response
 import           LN.T.User.Sanitized.Response
@@ -23,6 +24,8 @@ data GroupPackResponse = GroupPackResponse {
   groupPackResponseUserId         :: Int64,
   groupPackResponseGroup          :: GroupResponse,
   groupPackResponseGroupId        :: Int64,
+  groupPackResponseOrganization   :: OrganizationResponse,
+  groupPackResponseOrganizationId :: Int64,
   groupPackResponseStat           :: GroupStatResponse
 } deriving (Eq, Ord, Show, Read, Generic, Typeable, Out)
 
