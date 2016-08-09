227160bd818d58c1f2bdd7da9631b6ba0f0c9dd4
Andrew Darqui <andrew.darqui@gmail.com>
2016-08-09 00:03:25 -0400

    a few new jobs

--- src/LN/T/Job.hs
+++ src/LN/T/Job.hs
@@ -24,6 +24,10 @@ data Job
 data Queue
   = QNop
   | QPing
-  | QCreateUserProfile
-  | QCreateUserApi
+  | QCreateUserProfile       -- ^ When a user is created, it fires off a job to create a UserProfile
+  | QCreateUserApi           -- ^ When a user is created, it fires off a job to create an Api entry
+  | QAddThreadPostToSet      -- ^ Adds a thread post to a thread set
+  | QRemoveThreadPostFromSet -- ^ Deletes a thread post from a thread set
+  | QFixUserProfiles         -- ^ Adds profiles for users that don't have any
+  | QFixThreadPostSets       -- ^ Goes through all threads in the system and makes sure their sets are accurate
   deriving (Eq, Ord, Enum, Show, Read, Generic, Typeable)
