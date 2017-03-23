a5c4e934a1b62cf98aa505dad29c14cdc6b33b7d
Andrew Darqui <andrew.darqui@gmail.com>
2017-03-23 05:22:34 -0400

    ghc 8.x updates

--- src/LN/T/PatternSynonyms.hs
+++ src/LN/T/PatternSynonyms.hs
@@ -1,4 +1,5 @@
 {-# LANGUAGE PatternSynonyms #-}
+{-# LANGUAGE RankNTypes      #-}
 
 module LN.T.PatternSynonyms where
 
@@ -8,7 +9,20 @@ import Prelude
 
 
 
+pattern N :: forall t. Maybe t
 pattern N   = Nothing
+
+
+
+pattern J :: forall t. t -> Maybe t
 pattern J a = Just a
+
+
+
+pattern L :: forall t u. u -> Either u t
 pattern L a = Left a
+
+
+
+pattern R :: forall t u. u -> Either t u
 pattern R a = Right a
